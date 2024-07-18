537c629afa6c93dc9231d40c1185e9276608fbf7
tareqkb24-meet <t32185278@gmail.com>
2024-07-18 10:02:40 +0300

    day5

--- 0.6. Advanced Flask fortune-teller/app.py
+++ 0.6. Advanced Flask fortune-teller/app.py
@@ -9,7 +9,8 @@ def home():
         return render_template("hello.html")
     else:
         month = request.form['birthday']
-        return redirect(url_for('fortune',x=month ))
+        return redirect(url_for('fortune',x=month),'<h1>I’m not sure what will happen to you, but it will be one of the following three things:</h1>')
+
 
 
 fortune_list=["You will find money in the pocket of a coat you haven't worn in years.",
@@ -23,7 +24,7 @@ fortune_list=["You will find money in the pocket of a coat you haven't worn in y
 "You'll find the last slice of pizza hidden in the fridge, untouched and waiting for you.",
 "Your phone will autocorrect a serious message into a hilarious meme, and everyone will love it."]
 
-@app.route('/fortune_telling/<birthM>')
+@app.route('/fortune_telling/<x>')
 def fortune(x):
     
     birthLength = len(x)
@@ -31,8 +32,21 @@ def fortune(x):
     if birthLength<10:
         finalFortune = fortune_list[birthLength-1]
     return render_template("fortune.html", fortune = finalFortune)
-
-
+@app.route('/indecisive')
+def indecisive():
+    fortunelist2=["You will find money in the pocket of a coat you haven't worn in years.",
+"Your car will magically avoid potholes today.",
+"You will discover the secret to perfect microwave popcorn.",
+"A typo in your favor will lead to an amusing email exchange.",
+"You'll impress everyone with your flawless dance moves at an unexpected event.",
+"A bird will mistake you for a Disney princess and serenade you.",
+"Your pet will learn a new trick that will leave you in stitches.",
+"You will accidentally match outfits with a stranger and become temporary best friends.",
+"You'll find the last slice of pizza hidden in the fridge, untouched and waiting for you.",
+"Your phone will autocorrect a serious message into a hilarious meme, and everyone will love it."]
+    
+    indecisive_fortunes = random.sample(fortunes, 3)
+    return render_template('indecisive.html', fortunes=indecisive_fortunes)
 
 
     
