1e0d4da0cd8b91cebe041ed83f2e7c1d43ad3792
tareqkb24-meet <t32185278@gmail.com>
2024-07-20 01:30:26 +0300

    advanced flask

--- 0.6. Advanced Flask fortune-teller/app.py
+++ 0.6. Advanced Flask fortune-teller/app.py
@@ -6,12 +6,15 @@ app = Flask(__name__, template_folder='templates', static_folder='static')
 @app.route('/home', methods = ['GET', 'POST'])
 def home():
     if request.method == 'GET':
-        return render_template("hello.html")
+        return render_template("hello.html",)
     else:
-        month = request.form['birthday']
-        return redirect(url_for('fortune',x=month),'<h1>I’m not sure what will happen to you, but it will be one of the following three things:</h1>')
+        birthmonth = request.form['birthday']
+        return redirect(url_for('fortune', birthmonth=birthmonth),'<h1>I’m not sure what will happen to you, but it will be one of the following three things:</h1>')
 
-
+@app.route('/fortune2')
+def fortune2():
+    return render_template("fortune2.html")
+    
 
 fortune_list=["You will find money in the pocket of a coat you haven't worn in years.",
 "Your car will magically avoid potholes today.",
@@ -23,15 +26,15 @@ fortune_list=["You will find money in the pocket of a coat you haven't worn in y
 "You will accidentally match outfits with a stranger and become temporary best friends.",
 "You'll find the last slice of pizza hidden in the fridge, untouched and waiting for you.",
 "Your phone will autocorrect a serious message into a hilarious meme, and everyone will love it."]
-
-@app.route('/fortune_telling/<x>')
-def fortune(x):
-    
-    birthLength = len(x)
-    finalFortune = fortune_list[5]
+@app.route('/fortune_telling/<birthmonth>')
+def fortune(birthmonth):
+    birthLength = len(birthmonth)
+    finalFortune = fortune_list[9]
     if birthLength<10:
         finalFortune = fortune_list[birthLength-1]
     return render_template("fortune.html", fortune = finalFortune)
+
+
 @app.route('/indecisive')
 def indecisive():
     fortunelist2=["You will find money in the pocket of a coat you haven't worn in years.",
@@ -45,10 +48,10 @@ def indecisive():
 "You'll find the last slice of pizza hidden in the fridge, untouched and waiting for you.",
 "Your phone will autocorrect a serious message into a hilarious meme, and everyone will love it."]
     
-    indecisive_fortunes = random.sample(fortunes, 3)
+    indecisive_fortunes = random.sample(fortunelist2, 3)
     return render_template('indecisive.html', fortunes=indecisive_fortunes)
 
 
-    
+
 if __name__ == '__main__':
     app.run(debug=True)
